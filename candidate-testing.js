185f3e1e999878e67e065bc430ca1c21faea82af
Derek Hamilton <dhamiltonstl@gmail.com>
2024-03-26 16:07:13 -0500

    bug fixes

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,28 +3,44 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"
+];
+let candidateAnswers = [];
 
 
 const askForName = () => candidateName = input.question("What is your name? ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  question = "What year is it? ";
-  correctAnswer = "2024";
   candidateAnswer = input.question(question);
 
 }
+
+// const askQuestions = () => {
+//   for (i in questions) {
+//     candidateAnswers[i] = input.question(questions[i])
+//   }
+// }
 
 function gradeQuiz(candidateAnswers) {
   console.log(`Candidate Name: ${candidateName}`);
@@ -32,11 +48,18 @@ function gradeQuiz(candidateAnswers) {
   if (candidateAnswer == correctAnswer) console.log("Correct!");
   else console.log(`Incorrect.\nCorrect Answer: ${correctAnswer}`);
 
+  let grade = 0;  //TODO 3.2 use this variable to calculate the candidates score.
+  // for (i = 0; i < questions.length; i++) {
 
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+  //   let qNumb = i + 1
+  //   console.log(`${qNumb}. ${questions[i]}\nYour Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}`)
+  //   if (candidateAnswers[i] == correctAnswers[i]) {
+  //     grade++;
+  //   }
+  // }
 
 
-  return grade;
+  return grade; 
 }
 
 function runProgram() {
